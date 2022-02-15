2d89886529d3e8af718ad9adee9ccdf01a53514c
Armelle HENRY_Clo81 <clotildearmelle@gmail.com>
2022-02-15 13:58:42 +0100

    15/02/2022

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
 
 //-----------------QUIZZ-----------------------------
 
-// let sing = document.getElementsByClassName("chanteuse");
-// let song = document.getElementsByClassName("feat");
+let sing = document.getElementsByClassName("chanteuse").value;
+let song = document.getElementsByClassName("feat").value;
 
-//     console.log(sing[2]);
-//     console.log(song[0]);
  
 
 let btnQuizz = document.getElementById('quizz');
-    btnQuizz.addEventListener('click', function(){ 
-    alert(`Quel talent!`); 
+    btnQuizz.addEventListener('click', function($sing,$song){ 
+    alert(`Quel talent! {$sing} est terrible dans {$song}`); 
 });   
 
 
 //-----------FORMULAIRE------------------
    
 let btnValid = document.getElementById('btn');
-let psd = document.getElementById('pseudo'),
-    email = document.getElementById ('mail').value;       
+let x = document.getElementById('pseudo').value,
+    y = document.getElementById ('mail').value;  
+    
 
-btnValid.addEventListener('click', function(){    
+function change(x){
+    x = new x.innerHTML.value
+};
+
+function change(y){
+    y = new y.innerHTML.value
+};
+
+
+    btnValid.addEventListener('click', function(){        
     document.getElementById('alert').innerHTML = 
-    `Bonjour ${psd.value} Bienvenue sur ton espace Black Pink! Reçois des news de tes chanteuses préférées sur ${email.innerHTML} `
+    `Bonjour ${x.value} Bienvenue sur ton espace Black Pink! Reçois des news de tes chanteuses préférées sur ${y.value}`
 });
