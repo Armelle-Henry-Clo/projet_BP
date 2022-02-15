f4d003b3f79d5538a8d893ef53ffe5c906fc31a5
Armelle HENRY_Clo81 <clotildearmelle@gmail.com>
2022-02-15 20:22:18 +0100

    formulaire

--- js/app.js
+++ js/app.js
@@ -1,34 +1,21 @@
 
-//-----------------QUIZZ-----------------------------
+let sing = document.getElementsByName('chanteuse');
+    song = document.getElementsByName('checkbox');
+    title = document.getElementById('titleJs');
+    btnQuizz = document.getElementById('quizz');
+    btnValid = document.getElementById('btn'),
+    x = document.getElementById('pseudo');
+    y = document.getElementById ('mail');  
+    
+function change(){    
+    new this.innerHTML.value; 
+};
 
-let sing = document.getElementsByClassName("chanteuse").value;
-let song = document.getElementsByClassName("feat").value;
-
- 
-
-let btnQuizz = document.getElementById('quizz');
-    btnQuizz.addEventListener('click', function($sing,$song){ 
-    alert(`Quel talent! {$sing} est terrible dans {$song}`); 
+btnQuizz.addEventListener('click', function(){ 
+    document.getElementById('titleJs').innerHTML = `Quel talent! ${sing} est terrible dans ${song}.`
 });   
 
-
-//-----------FORMULAIRE------------------
-   
-let btnValid = document.getElementById('btn');
-let x = document.getElementById('pseudo').value,
-    y = document.getElementById ('mail').value;  
-    
-
-function change(x){
-    x = new x.innerHTML.value
-};
-
-function change(y){
-    y = new y.innerHTML.value
-};
-
-
-    btnValid.addEventListener('click', function(){        
+btnValid.addEventListener('click', function(){        
     document.getElementById('alert').innerHTML = 
     `Bonjour ${x.value} Bienvenue sur ton espace Black Pink! Reçois des news de tes chanteuses préférées sur ${y.value}`
 });
