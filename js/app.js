4ff6285e6e3a43cfca6722e9f6685329612b2a9f
Armelle HENRY_Clo81 <clotildearmelle@gmail.com>
2022-02-16 00:42:25 +0100

    prompt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//-----Selecteurs---------------
 let sing = document.getElementsByTagName('chanteuse')["Jennie" ,"Jisoo","Rosé","Lisa"];
     song = document.getElementsByName('feat')['Kiss','BetYourWanna','IceCream','NoMore'];
     title = document.getElementById('titleJs');
@@ -7,21 +7,36 @@ let sing = document.getElementsByTagName('chanteuse')["Jennie" ,"Jisoo","Rosé",
     x = document.getElementById('pseudo');
     y = document.getElementById ('mail');  
     
+//Quizz---------------------------
+
+// function myChange(){
+//     sing = 
+//     console.log(this.value);
+// }
+
+btnQuizz.addEventListener('click', function(){ 
+    btnQuizz.innerHTML = `Quel talent!`
+});  
+
+//----Formulaire----------------
+
 function change(){    
     new this.innerHTML.value; 
 };
 
-function mychange(){
-    switch
-}
-
-console.log(sing,song);
-
-btnQuizz.addEventListener('click', function(){ 
-    document.getElementById('titleJs').innerHTML = `Quel talent! ${sing} est terrible dans ${song}.`
-});   
-
-btnValid.addEventListener('click', function(){        
+btnValid.addEventListener('click', function(){ 
+    if(x.value != 0 & y.value != 0){       
     document.getElementById('alert').innerHTML = 
     `Bonjour ${x.value} Bienvenue sur ton espace Black Pink! Reçois des news de tes chanteuses préférées sur ${y.value}`
-});
+    }else{
+        document.getElementById('alert').innerHTML = 'Pas encore inscrit? Cliquez au dessus!';   
+    }
+})
+
+
+function first(){
+    let resultatPrompt=prompt("Entrez votre email");
+    document.getElementById("msg").innerHTML = resultatPrompt;
+
+    alert (`Bravo! Bientôt des news sur ton mail: ${resultatPrompt}`);
+};
